b3d7e8f100035b0053df3a56505c64293334296c
Qingyang Lan <qingyanglan@Mac.attlocal.net>
2025-10-15 18:33:17 -0400

    Video: https://drive.google.com/file/d/1YRMZwQDUv0TSze4fMI0azqgFf4BL0uHr/view?usp=drive_link

--- Project 1.py
+++ Project 1.py
@@ -7,7 +7,7 @@
 # Calculation I:Qingyang Lan Calculation II: Adam Weng Calculation III: Yufan Xu
 # Main coding: Qingyang Lan, Adam Weng Diagram: Yufan Xu
 # GenAI Usage: Asked ChatGPT to help debug, format.
-# Video upload in google drive
+# [Click here to watch on Google Drive]（https://drive.google.com/file/d/1YRMZwQDUv0TSze4fMI0azqgFf4BL0uHr/view?usp=drive_link）
 # =========================================================
 
 import csv
